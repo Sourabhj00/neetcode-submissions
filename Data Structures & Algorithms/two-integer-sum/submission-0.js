ac263f70ee1f9d7dd72cdb1f20d40c86b176ad71
Sourabh Jain <53055723+Sourabhj00@users.noreply.github.com>
2026-06-25 00:19:04 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map();
+        for(let i=0; i< nums.length;i++){
+            if(map.has(nums[i])){
+                // const first = map.get(nums[i]);
+                // const second = i;
+                // if(first > second){
+                //     [first,second] = [second,first];
+                // }
+                return [map.get(nums[i]),i];
+            }
+            map.set(target - nums[i],i);
+        }
+    }
+}
