baf8b359564405cae2663a01b67ba49ebeab16d3
Sourabh Jain <53055723+Sourabhj00@users.noreply.github.com>
2026-09-09 01:34:08 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.js
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[][]}
+     */
+    solveNQueens(n) {
+        const board = Array.from({length: n}, () => Array(n).fill('.'));
+        const res = [];
+        this.NQueens(board, res, n, 0);
+        return res;
+    }
+
+    NQueens(board, res, n, row){
+        if(row == n){
+            res.push(board.map(row => row.join('')));
+            return;
+        }
+        for(let i=0; i<n; i++){
+            if(this.isSafe(board, n, row, i)){
+                board[row][i] = 'Q';
+                this.NQueens(board,res,n, row + 1);
+                board[row][i] = '.';
+            }
+        }
+    }
+
+    isSafe(board,n, row, col){
+        for(let i=0; i<n; i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        for(let i=row-1, j=col-1; i >=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        for(let i=row-1, j=col+1; i>= 0 && j < n; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
